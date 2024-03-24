ca2021eb63afb3ff279920d3e9a8da222be0cb40
Ian Estrada <144106133+Extrias@users.noreply.github.com>
2024-03-24 17:52:25 -0600

    Update tokenizer.c

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -20,3 +20,26 @@ int non_space_char(char c){
 }
 
 
+char *token_start(char *s) {
+    while (*s && (*s == ' ' || *s == '\t' || *s == '\n' || *s == '\r')) {
+        s++;
+    }
+
+    if (*s == '\0') {
+        return NULL;
+    }
+
+    return s;
+}
+
+char *token_terminator(char *token) {
+    // Find the end of the token
+    while (*token && !(*token == ' ' || *token == '\t' || *token == '\n' || *token == '\r')) {
+        token++;
+    }
+
+    // Return pointer to the terminator character
+    return token;
+}
+
+
